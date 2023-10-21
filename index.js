93b10378a32c999f6dcb2c9a5648b29c0766d793
Devansh Rajput <rajputdevansh012@gmail.com>
2023-10-21 18:12:26 +0530

    Update file

--- index.js
+++ index.js
@@ -1,51 +1,124 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const nodemailer = require("nodemailer");
 
 const app = express();
 const PORT = 7000;
 
 mongoose.connect("mongodb://localhost:27017/Hotel-Management-System")
-.then(()=>console.log("MongoDb connected"))
-.catch((err)=>console.log("MongoDb Error , err"));
+  .then(() => console.log("MongoDb connected"))
+  .catch((err) => console.log("MongoDb Error", err));
 
 //Schema
-
 const userSchema = new mongoose.Schema({
-    firstName:{
-        type: String,
-        required: true,     
-    }, 
-    lastName:{
-        type: String,
-    },
-    email:{
-        type: String,
-        required: true,
-    },
-    accounttype:{
-        type: String,
-        required: true,
-    },
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  accounttype: {
+    type: String,
+    enum: ['Customer', 'Hotel Manager', 'Admin'],
+    default: 'Customer', 
+   },
+   password:{
+    type:String,
+    required:true,
+   }
 });
 
-const User = mongoose.model("user",userSchema);
+const roomSchema = new mongoose.Schema({
+  hotelId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Hotel',
+    required: true,
+  },
+  roomNumber: {
+    type: String,
+    required: true,
+  },
+  roomType: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  availability: {
+    type: String,
+    enum: ['Available', 'Booked'],
+    default: 'Available',
+  },
+  bookings: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Booking',
+  }],
+});
 
-//Middleware
+const bookingSchema = new mongoose.Schema({
+  customerID: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  roomID: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Room',
+    required: true,
+  },
+  checkInDate: {
+    type: Date,
+    required: true,
+  },
+  checkOutDate: {
+    type: Date,
+    required: true,
+  },
+  status: {
+    type: String,
+    enum: ['Reserved', 'Checked-In', 'Checked-Out'],
+    default: 'Reserved',
+  },
+});
+
+const User = mongoose.model("user", userSchema);
+const Room = mongoose.model("room", roomSchema);
+const Booking = mongoose.model("booking", bookingSchema);
+
+
+// Middleware
 app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
-//Rest Api
-  
-  // Add a User
-  app.post('/api/User', (req, res) => {
-   
-  });
-  
+// Rest Api
 
-const userData = await User.create({
-    firstName:body.firstName, 
-    lastName:body.lastName,
-    email:body.email,
-    accounttype:body.accounttype,
-})
+// Add a User
+app.post('/signup', async (req, res) => {
+  try {
+    const { firstName, lastName, email, accounttype } = req.body;
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    const userData = await User.create({
+      firstName,
+      lastName,
+      email,
+      accounttype,
+      password: hashedPassword,
+    });
+    res.status(201).json(userData);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Server Error');
+  }
+});
 
 app.listen(PORT, () => console.log(`Server started at port:${PORT}`));
