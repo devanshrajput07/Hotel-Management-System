4abd98de77806cba37089f658e1cc2a5f647d812
Devansh Rajput <rajputdevansh012@gmail.com>
2023-10-27 22:12:18 +0530

    Update files

--- models/User.js
+++ models/User.js
@@ -5,28 +5,22 @@ const userSchema = new mongoose.Schema({
     firstName: {
       type: String,
       required: true,
-      trim:true
     },
     lastName: {
       type: String,
-      trim:true
     },
     email: {
       type: String,
       required: true,
-      trim:true
     },
     accounttype: {
       type: String,
-      required:true,
-      enum: ['Customer', 'Hotel Manager', 'Admin'],
+      enum: ['Customer', 'Hotel-Manager', 'Admin'],
       default: 'Customer',
-      trim:true
     },
     password: {
       type: String,
       required: true,
-      trim:true
     }
   });
 
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,5 +4,6 @@ import UserController from '../controllers/userController.js';
 
 // Public Routes
 router.post('/signup', UserController.userRegistration)
+router.post('/login', UserController.userLogin)
 
 export default router
