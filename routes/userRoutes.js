dc6077b75e0815c285d6d84c01b0b3018b49179b
Devansh Rajput <rajputdevansh012@gmail.com>
2023-10-27 22:46:40 +0530

    Update files

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,5 +5,7 @@ import UserController from '../controllers/userController.js';
 // Public Routes
 router.post('/signup', UserController.userRegistration)
 router.post('/login', UserController.userLogin)
+router.post('/send-reset-password-email', UserController.sendUserPasswordResetEmail)
+router.post('/reset-password/:id/:token', UserController.userPasswordReset)
 
 export default router
