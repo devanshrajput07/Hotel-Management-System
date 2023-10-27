2054353615c4a26e2a7a1648c2528026e1b529ae
Devansh Rajput <rajputdevansh012@gmail.com>
2023-10-27 22:11:48 +0530

    Update files

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,36 +4,62 @@ import jwt from 'jsonwebtoken'
 
 class UserController {
     static userRegistration = async (req, res) => {
-      const { firstName, lastName, email, accounttype, password} = req.body
-      const user = await UserModel.findOne({ email: email })
-      if (user) {
-        res.send({ "status": "failed", "message": "Email already exists" })
-      } else {
-        if (firstName && lastName && email && accounttype && password) {
-            try {
-              const salt = await bcrypt.genSalt(10)
-              const hashPassword = await bcrypt.hash(password, salt)
-              const doc = new UserModel({
-                firstName: firstName,
-                lastName: lastName,
-                email: email,
-                accounttype: accounttype,
-                password: hashPassword,
-              })
-              await doc.save()
-              const saved_user = await UserModel.findOne({ email: email })
-              // Generate JWT Token
-              const token = jwt.sign({ userID: saved_user._id }, process.env.JWT_SECRET_KEY, { expiresIn: '5d' })
-              res.status(201).send({ "status": "success", "message": "Registration Success", "token": token })
-            } catch (error) {
-              console.log(error)
-              res.send({ "status": "failed", "message": "Unable to Register" })
+        const { firstName, lastName, email, accounttype, password } = req.body
+        const user = await UserModel.findOne({ email: email })
+        if (user) {
+            res.send({ "status": "failed", "message": "Email already exists" })
+        } else {
+            if (firstName && lastName && email && accounttype && password) {
+                try {
+                    const salt = await bcrypt.genSalt(10)
+                    const hashPassword = await bcrypt.hash(password, salt)
+                    const doc = new UserModel({
+                        firstName: firstName,
+                        lastName: lastName,
+                        email: email,
+                        accounttype: accounttype,
+                        password: hashPassword,
+                    })
+                    await doc.save()
+                      const saved_user = await UserModel.findOne({ email: email })
+                    //   // Generate JWT Token
+                      const token = jwt.sign({ userID: saved_user._id }, process.env.JWT_SECRET_KEY, { expiresIn: '5d' })
+                    res.status(201).send({ "status": "success", "message": "Registration Success","token": token})
+                } catch (error) {
+                    console.log(error)
+                    res.send({ "status": "failed", "message": "Unable to Register" })
+                }
+            } else {
+                res.send({ "status": "failed", "message": "All fields are required" })
             }
-        }else{
-          res.send({ "status": "failed", "message": "All fields are required" })
+        }
+    }
+
+    static userLogin = async (req, res) => {
+        try {
+            const { email, password } = req.body
+            if (email && password) {
+                const user = await UserModel.findOne({ email: email })
+                if (user != null) {
+                    const isMatch = await bcrypt.compare(password, user.password)
+                    if ((user.email === email) && isMatch) {
+                        // Generate JWT Token
+                        const token = jwt.sign({ userID: user._id }, process.env.JWT_SECRET_KEY, { expiresIn: '5d' })
+                        res.send({ "status": "success", "message": "Login Success","token": token})
+                    } else {
+                        res.send({ "status": "failed", "message": "Email or Password is not Valid" })
+                    }
+                } else {
+                    res.send({ "status": "failed", "message": "You are not a Registered User" })
+                }
+            } else {
+                res.send({ "status": "failed", "message": "All Fields are Required" })
+            }
+        } catch (error) {
+            console.log(error)
+            res.send({ "status": "failed", "message": "Unable to Login" })
         }
     }
 }
-}
 
-export default userController
+export default UserController
