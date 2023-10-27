29c30387230c0952570e7798d16e66f848b35114
Devansh Rajput <rajputdevansh012@gmail.com>
2023-10-27 20:39:19 +0530

    Update files

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ dotenv.config()
 import express from 'express'
 import cors from 'cors'
 import connectDB from './config/connectDb.js'
-// import userRoutes from './routes/userRoutes.js'
+import userRoutes from './routes/userRoutes.js'
 
 const app = express()
 const port = process.env.PORT
@@ -20,7 +20,7 @@ connectDB(DATABASE_URL)
 app.use(express.json())
 
 // Load Routes
-// app.use("/api/user", userRoutes)
+app.use("/api/user", userRoutes)
 
 app.listen(port, () => {
     console.log(`Server listening at http://localhost:${port}`)
